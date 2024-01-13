95fa7e539bca4740afcdd0aaba53afb6e69e6d7c
TOICTONY <114583077+TOICTONY@users.noreply.github.com>
2024-01-13 23:01:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from pyrogram import Client, filters
 
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+Shellbot = Client(
+  name="shellbot",
+  api_id=API_ID,
+  api_hash=API_HASH,
+  bot_token=BOT_TOKEN
+)
+
+print('bot was started")
+
+Shellbot.run()    
